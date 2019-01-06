35658d8656ee801f048c0018560b414d2e709bbf
oesebus <idrissisolutions@gmail.com>
2019-01-06 16:53:58 +0100

    Add palindrome test scenario

--- localFunctions/Program.cs
+++ localFunctions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace localFunctions
 {
@@ -10,8 +11,8 @@ namespace localFunctions
 			Console.WriteLine("Hello World!");
 			Console.Read();
 
-			IEnumerable<string> res = FormatStrings(new string[] { "Je", "suis", "Ismael" }, 0, 2);
-
+			IEnumerable<string> res = FormatStrings(new string[] { "Je", "suis", "Ismael" }, 3, 2);
+			// Todo type some logic here ....
 			foreach (var item in res)
 			{
 				Console.Write(item);
@@ -27,15 +28,40 @@ namespace localFunctions
 			if (fin > strs.Length - 1)
 				throw new ArgumentException($"{nameof(fin)}");
 
-			return getLocal(strs, debut, fin);
+			return GetLocal(strs, debut, fin);
 
-			IEnumerable<string> getLocal(string[] array, int start, int end)
+
+			IEnumerable<string> GetLocal(string[] array, int start, int end)
 			{
-				for (int i = debut; i <= fin; i++)
+				for (int i = start; i <= end; i++)
 				{
-					var res = $"{strs[i]} ";
+					var res = $"{array[i]} ";
 					yield return res;
 				}
+			}
+		}
+
+		public static bool IsWordPalindrome(string word)
+		{
+			if (word is null)
+				throw new ArgumentException($" Invalid argument => {nameof(word)}");
+
+			if (word.Length < 2)
+				return true;
+
+			return IsPalindrome(0, word.Length - 1);
+
+			bool IsPalindrome(int lo, int hi)
+			{
+				if (lo >= hi)
+				{
+					return true;
+				}
+
+				if (char.ToUpperInvariant(word[lo]) != char.ToUpperInvariant(word[hi]))
+					return false;
+
+				return IsPalindrome(lo + 1, hi + 1);
 			}
 		}
 	}
